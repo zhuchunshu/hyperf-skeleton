4fc67f77356a50e931ae6c5e7ac38b112b222601
zhuchunshu <laravel@88.com>
2021-07-01 16:39:30 +0800

    更新:新增生成分页视图函数

--- app/helpers.php
+++ app/helpers.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 use Hyperf\Utils\Context;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
+use Hyperf\Paginator\UrlWindow;
 use Hyperf\View\RenderInterface;
 use App\CodeFec\Menu\MenuInterface;
 use Hyperf\Utils\ApplicationContext;
@@ -392,5 +393,20 @@ if (!function_exists('get_client_ip')) {
             return $ip_addr;
         }
         return '0.0.0.0';
+    }
+}
+
+if(!function_exists("make_page")){
+    function make_page($page,$default = "default"){
+        $window = UrlWindow::make($page);
+
+        $elements = array_filter([
+            $window['first'],
+            is_array($window['slider']) ? '...' : null,
+            $window['slider'],
+            is_array($window['last']) ? '...' : null,
+            $window['last'],
+        ]);
+        return view("shared.page.".$default,['paginator' => $page,'elements' => $elements]);
     }
 }
